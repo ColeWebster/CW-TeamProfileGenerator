483381db2fa37b3a55f36ed0dee72a1521bbe2bb
Cole's Laptop <cole.webster@gmail.com>
2022-01-25 16:08:55 -0500

    Adding updated lib files for each role and updated index to generate the HTML

--- index.js
+++ index.js
@@ -1,26 +1,51 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const questions = require('./utils/questions');
 const generateHTML = require('./utils/generateHTML');
+
+const {managerQuestions, engineerQuestions, internQuestions} = require('./utils/questions');
 // Import Employee Manager Engineer Intern
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const { create } = require('domain');
 
 
+// const newPage = async () => {
+//     const answers = await inquirer.prompt(questions);
+//     const htmlContent = generateHTML(answers);
+//     writeToFile(htmlContent);
+// };
 
-const newPage = async () => {
-    const answers = await inquirer.prompt(questions);
-    const htmlContent = generateHTML(answers);
-    writeToFile(htmlContent);
-};
+// const writeToFile = (htmlDirectContent) => {
+//     fs.writeFile("test/index.html", htmlDirectContent, (err) => 
+//         err ? console.log(err) : console.log('Created successfully!')
+//     )
+// }
 
-const writeToFile = (htmlDirectContent) => {
-    fs.writeFile("test/index.html", htmlDirectContent, (err) => 
-        err ? console.log(err) : console.log('Created successfully!')
-    )
+// newPage();
+
+menu = () => {
+    createManager = async () => {
+        const responses = await inquirer.prompt(managerQuestions);
+        console.log(answers);
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+    },
+    createEngineer = async () => {
+        const responses = await inquirer.prompt(engineerQuestions);
+        console.log(answers);
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+    },
+    createManager = async () => {
+        const responses = await inquirer.prompt(internQuestions);
+        console.log(answers);
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.university);
+    }    
+
+    createManager();
+    createEngineer();
+    createIntern();
 }
-
-newPage();
-
-
 
 // Call manager prompt
 
