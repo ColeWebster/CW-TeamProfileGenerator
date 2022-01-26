1c7fce159b1256cd8f961ad4380add2013e2c099
Cole Webster <Cole.Webster@gmail.com>
2022-01-25 21:29:50 -0500

    Updated index file to include prompts per role

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Manager = require('./lib/Manager');
-// const Engineer = require('./lib/Engineer');
-// const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 // const {managerQuestions, engineerQuestions, internQuestions} = require('./utils/questions');
 const inquirer = require('inquirer');
 const fs = require('fs');
@@ -28,17 +28,77 @@ menu = () => {
                 },
                 {
                     type: 'input',
-                    name: 'officeNumber',
+                    name: 'office',
                     message: 'Please add the team managers office number:'
                 },
             ])
-            .then(({firstName,id,email,officenumber}) => {
-                const manager = new Manager(firstName, id, email, officenumber);
+            .then(({ firstName, id, email, office }) => {
+                const manager = new Manager(firstName, id, email, office);
                 console.log(manager)
             });
     };
-    
+    createEngineer = () => {
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'engineerName',
+                    message: 'Please enter the engineers name:',
+                },
+                {
+                    type: 'input',
+                    name: 'engineerID',
+                    message: 'Please enter the engineers employee ID number:'
+                },
+                {
+                    type: 'input',
+                    name: 'engineerEmail',
+                    message: 'Please add the team managers email address:'
+                },
+                {
+                    type: 'input',
+                    name: 'github',
+                    message: 'Please add the engineers gitHub:'
+                },
+            ])
+            .then(({ engineerName, engineerID, engineerEmail, github}) => {
+                const engineer = new Engineer(engineerName, engineerID, engineerEmail, github);
+                console.log(engineer);
+            })
+    };
+    createIntern = () => {
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'internFirstName',
+                    message: 'Please enter the interns name:',
+                },
+                {
+                    type: 'input',
+                    name: 'internID',
+                    message: 'Please enter the interns employee ID number:'
+                },
+                {
+                    type: 'input',
+                    name: 'internEmail',
+                    message: 'Please add the interns email address:'
+                },
+                {
+                    type: 'input',
+                    name: 'university',
+                    message: 'Please add the interns current school:'
+                },
+            ])
+            .then(({internFirstName, internID, internEmail, university}) => {
+                const intern = new Intern(internFirstName, internID, internEmail, university);
+                console.log(intern);
+            })
+    }
+
     createManager();
+    createEngineer();
+    createIntern();
 }
 
 menu();
